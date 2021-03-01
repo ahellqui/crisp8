9b9cc79e802033ceecfeaef4a1aec75b7bb97ac3
Andreas Hellquist <ahellqui@gmail.com>
2021-03-01 20:43:53 +0100

    Filled the chip8_s struct
    Filled the chip8_s struct with all the information it will need to
    emulate stuff.

--- crisp8/crisp8.c
+++ crisp8/crisp8.c
@@ -1,10 +1,14 @@
 #include "crisp8.h"
 
-// I'm doing my best to avoid heap allocations.
+#include "stack.h"
+
 #define MEMORY_SIZE 4096
 
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
+
+typedef void (*audioCallback) (void);
+typedef uint16_t (*inputCallback) (void);
 
 struct chip8_s
 {
@@ -14,11 +18,11 @@ struct chip8_s
     uint8_t memory [MEMORY_SIZE];
     uint8_t display [DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
-    // Stack to be added
+    chip8Stack stack;
 
     // Registers ---------------------
 
-    uint8_t PC;
+    uint16_t PC;
     uint16_t I;
     // Using an array allows us to more easily choose registers from opcodes
     uint8_t V [16];
@@ -28,5 +32,12 @@ struct chip8_s
     uint8_t delay_timer;
     uint8_t sound_timer;
 
-    // Callbacks to be added
+    // Non emulator information ------
+
+    // Callbacks
+    audioCallback audioCb;
+    inputCallback inputCb;
+
+    // Framerate
+    uint16_t framerate;
 };

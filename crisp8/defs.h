be24e62d9740c3ed332c66fc12ec96002f2c300b
Andreas Hellquist <ahellqui@gmail.com>
2021-03-04 20:32:25 +0100

    Add font loading
    Adds font loading upon initialization of the chip-8 emulator. In order
    to aid the implementation of future functionality, a new translation
    unit is introduced with defines necessary in more than one place.

---
+++ crisp8/defs.h
@@ -0,0 +1,9 @@
+#ifndef CRISP8_DEFINES
+#define CRISP8_DEFINES 
+
+
+// The font needs to be located anywhere before 0x200, but apparently it's convention to start
+// it between 0x50 and 0x9F so I'll follow that
+#define FONT_START_ADDRESS 0x50
+
+#endif
